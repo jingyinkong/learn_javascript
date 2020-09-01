d74213c28697fe96115f59d0fb1cd3d502dfbe78
jingyinkong <18342205990@163.com>
2020-09-01 22:29:49 +0800

    day05

--- js/my.js
+++ js/my.js
@@ -1,6 +1,11 @@
-function test() {
-    console.log(arguments);
-    console.log(arguments.length);
+function Hero(name, type, blood) {
+    this.name = name;
+    this.type = type;
+    this.blood = blood;
+    this.attack = function(attack) {
+        console.log(attack);
+    }
 }
 
-test(1, 2, 3, 4, 5);
+var hero1 = new Hero('后裔', '射手', 100);
+hero1.attack('远程');
